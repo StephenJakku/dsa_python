6bf78c9313ca125ee4e520ae6737d9ad0a4a9f31
Stephen Jakku <uttamjakku96@gmail.com>
2022-02-03 15:11:18 -0800

    Stack prog added

---
+++ datastructures/stack.py
@@ -0,0 +1,35 @@
+class Stack():
+    def __init__(self):
+        self.items=[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1] if not self.isEmpty() else "No elements in the Stack"
+
+    def getStack(self):
+        return self.items
+
+    def isEmpty(self):
+        return True if len(self.items)<=0 else False
+
+myStack=Stack()
+myStack.push(1)
+myStack.push(2)
+myStack.push(3)
+myStack.push(4)
+print(myStack.peek())
+print(myStack.getStack())
+myStack.pop()
+myStack.pop()
+print(myStack.peek())
+print(myStack.getStack())
+print(myStack.isEmpty())
+myStack.pop()
+myStack.pop()
+print(myStack.isEmpty())
+print(myStack.peek())
